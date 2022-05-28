179776a6a2fdb99e8abadd3ba9252c5f2e48716c
Thillai Chithambaram <79466435+itzThillaiC@users.noreply.github.com>
2022-05-28 20:58:39 +0530

    Update body_part_angle.py

--- body_part_angle.py
+++ body_part_angle.py
@@ -15,11 +15,7 @@ class BodyPartAngle:
         l_wrist = detection_body_part(self.landmarks, "LEFT_WRIST")
         return calculate_angle(l_shoulder, l_elbow, l_wrist)
 
-    def angle_of_the_right_arm(self):
-        r_shoulder = detection_body_part(self.landmarks, "RIGHT_SHOULDER")
-        r_elbow = detection_body_part(self.landmarks, "RIGHT_ELBOW")
-        r_wrist = detection_body_part(self.landmarks, "RIGHT_WRIST")
-        return calculate_angle(r_shoulder, r_elbow, r_wrist)
+    
 
     def angle_of_the_left_leg(self):
         l_hip = detection_body_part(self.landmarks, "LEFT_HIP")
